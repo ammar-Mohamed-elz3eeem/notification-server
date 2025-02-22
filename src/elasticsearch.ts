0c43adfb54030f17fca3843dfa4e0e95e810e3f1
Ammar Massoud <ammarmohamedthez@gmail.com>
2025-02-22 02:27:43 +0200

    Add file to connect to elasticsearch

---
+++ src/elasticsearch.ts
@@ -0,0 +1,28 @@
+import { winstonLogger } from "@ammar-Mohamed-elz3eeem/protal-lab";
+import { Client } from "@elastic/elasticsearch";
+import { config } from "@notifications/config";
+
+const log = winstonLogger(
+  config.ELASTIC_SEARCH_URL || "",
+  'notification-elastic-search',
+  'debug'
+);
+
+const elasticSearchClient = new Client({
+  node: config.ELASTIC_SEARCH_URL
+});
+
+export const checkConnection = async (): Promise<void> => {
+  let isConnected = false;
+
+  while (!isConnected) {
+    try {
+      const health = await elasticSearchClient.cluster.health({});
+      log.info(`health status -- ${health.status}`);
+      isConnected = true;
+    } catch (error) {
+      log.error('Connection to elastic search failed. Retrying...');
+      log.log('error', '[NotificationElasticSearch] checkConnection():', error);
+    }
+  }
+};
